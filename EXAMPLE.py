24296ac890eaf0daa53f29399a3f388a74ba19f2
kosti <kosizmailov@yandex.ru>
2025-04-23 17:39:42 +0300

    fixed example.py

--- EXAMPLE.py
+++ EXAMPLE.py
@@ -11,8 +11,8 @@ import json
 import pprint
 
 
-def get_response(process_type: str, path2archive="sample.zip"):
-    response = requests.post("http://localhost:5000/archives/", files={"file": open(path2archive, "rb")}, params={"process_type": process_type})
+def get_response(process_type: str, path2archive="sample_yandex_contest.zip"):
+    response = requests.post("http://localhost:5000/api/archives/", files={"file": open(path2archive, "rb")}, params={"process_type": process_type})
     print(json.loads(response.content))
     return json.loads(response.content)["task_id"]
 
@@ -22,7 +22,7 @@ print(task_id)
 attempts = int(input("введите число попыток получения ответа от api: "))
 for i in range(attempts):
     print(f'{i + 1} attempt to respond')
-    status_response = requests.get(f"http://localhost:5000/archives/status/{task_id}")
+    status_response = requests.get(f"http://localhost:5000/api/status/{task_id}")
     pprint.pprint(json.loads(status_response.content))
     print('---------------\n')
     time.sleep(1.5)
