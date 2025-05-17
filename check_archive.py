7cd5866ffb4ab7637ee4c9ef2ffa4cf5ddc78696
kosti <kosizmailov@yandex.ru>
2025-05-17 20:57:54 +0300

    minor fix

--- check_archive.py
+++ check_archive.py
@@ -1,15 +1,27 @@
+import re
 from zipfile import ZipFile
 from rarfile import RarFile
 from tarfile import TarFile
 
+"""
+проверяет, является ли архив архивом яндекс контеста
+:param arc: объект архива
+:return: True/False
+"""
 
 def check_archive(arc: ZipFile | RarFile | TarFile) -> bool:
-    """
-    проверяет, является ли архив архивом яндекс контеста
-    :param arc: объект архива
-    :return: True/False
-    """
+
     paths = [i for i in arc.namelist()]
+    initial = paths[0].split('/')[0]
+    for path in paths[1:]:
+        print(path)
+        if initial in path.split('/')[0]:
+            if len(paths) - 1 == paths.index(path):
+                return False
+            continue
+        else:
+            break
+
     folders = set()
     if len(paths) != 0:
         for path in paths:
@@ -18,6 +30,7 @@ def check_archive(arc: ZipFile | RarFile | TarFile) -> bool:
             file = False
             for entry in arc.namelist():
                 parts = entry.split("/")
+
                 if parts[0] == d and len(parts) > 1:
                     if isinstance(arc, TarFile):
                         member = arc.getmember(entry)
